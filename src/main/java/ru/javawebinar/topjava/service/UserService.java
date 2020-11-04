6ddaf188d3fd3a1ceda0642f9652208c7a7995b4
sergey <sierghieigavrilov053@gmail.com>
2020-11-04 21:01:32 +0200

    not worked project

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
@@ -23,6 +24,7 @@ public class UserService {
 
 
     public User create(User user) {
+        Assert.notNull(user, "user must not be null");
         return repository.save(user);
     }
 
@@ -35,6 +37,7 @@ public class UserService {
     }
 
     public User getByEmail(String email) {
+        Assert.notNull(email, "email must not be null");
         return checkNotFound(repository.getByEmail(email), "email=" + email);
     }
 
@@ -43,6 +46,7 @@ public class UserService {
     }
 
     public void update(User user) {
-        checkNotFoundWithId(repository.save(user), user.getId());
+        Assert.notNull(user, "user must not be null");
+        checkNotFoundWithId(repository.save(user), user.id());
     }
 }
